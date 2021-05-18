36f7c6dc8c2b9c627dc2d9df2fa09dc9a5695ac4
JuriVitali <S1092588@studenti.univpm.it>
2021-05-18 19:57:51 +0200

    Primo commit

--- Home/VistaHome_amministratore.py
+++ Home/VistaHome_amministratore.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QPushButton, QSizePolicy, QLabel, QVBoxLayout, QSpacerItem
+from PyQt5 import QtCore
 from PyQt5.QtGui import QPixmap, QFont, QIcon
-
 
 class VistaHome_amministratore(QWidget):
 
@@ -8,53 +8,55 @@ class VistaHome_amministratore(QWidget):
         super(VistaHome_amministratore, self).__init__(parent)
         self.setWindowTitle("Home Amministratore")                      #titolo
         self.setStyleSheet("background-color : #333;")                 #colore dello sfondo
-        self.setGeometry(140, 80, 1000, 600)
 
-        ext_layout = QVBoxLayout()
+
+        self.ext_layout = QVBoxLayout()
 
         striscia = QLabel('  Ciak e Azione')
         striscia.setFont(QFont('Segoe Script', 38))
         striscia.setStyleSheet("color:  #00BF7F;"
-                               "background-color: #111;"
-                               "radius: 10px;"
-                               "offset: -3,-3"
+                               "background-color: #ddd;"
                                 )
         striscia.setSizePolicy(QSizePolicy.Expanding,QSizePolicy.Minimum)
         orizzontal_layout = QHBoxLayout()
 
         label = QLabel()                                                #label su cui deve essere caricata l'immagine
 
-        pixmap = QPixmap('Imm1.png')
-        label.setPixmap(pixmap)                                          #caricamento dell'immagine (che non funziona)
+        pixmap = QPixmap('Home\\Sfondo_amm2.jpg')        #caricamento dell'immagine
+        label.setPixmap(pixmap)
+        label.setScaledContents(True)
+        label.setContentsMargins(0,0,0,0)
         label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
         menu_pulsanti = QVBoxLayout()     #creazione del layout verticale che conterrà i pulsanti
         menu_pulsanti.addItem(QSpacerItem(10,18, QSizePolicy.Minimum, QSizePolicy.Expanding))
-        menu_pulsanti.addWidget(self.get_push_button("Dipendenti", self.go_to_dipendenti(), QIcon('icona_dipendenti.png'), 'Gestione dei dipendenti'))
-        menu_pulsanti.addWidget(self.get_push_button("Film", self.go_to_film(), QIcon('icona_film.jpg'), 'Lista dei film'))
-        menu_pulsanti.addWidget(self.get_push_button("Spettacoli", self.go_to_spettacoli(), QIcon('icona_spettacolo.png'), 'Programmazione degli spettacoli'))
-        menu_pulsanti.addWidget(self.get_push_button("Report", self.go_to_report(), QIcon('icona_report.png'), 'Visualizzazione dei report'))
-        menu_pulsanti.addWidget(self.get_push_button("Servizi", self.go_to_servizi(), QIcon('icona_biglietto.jpg'), 'Gestione dei servizi'))
+        menu_pulsanti.addWidget(self.get_push_button("Dipendenti", self.go_to_dipendenti, QIcon('Home\\dipendente_icon.png'), 'Gestione dei dipendenti'))
+        menu_pulsanti.addWidget(self.get_push_button("Film", self.go_to_film, QIcon('Home\\film_icon.png'), 'Lista dei film'))
+        menu_pulsanti.addWidget(self.get_push_button("Spettacoli", self.go_to_spettacoli, QIcon('Home\\spettacoli_icon.jpg'), 'Programmazione degli spettacoli'))
+        menu_pulsanti.addWidget(self.get_push_button("Report", self.go_to_report, QIcon('Home\\report_icon.png'), 'Visualizzazione dei report'))
+        menu_pulsanti.addWidget(self.get_push_button("Servizi", self.go_to_servizi, QIcon('Home\\servizi_icon.ico'), 'Gestione dei servizi'))
         menu_pulsanti.addItem(QSpacerItem(10, 20, QSizePolicy.Minimum, QSizePolicy.Expanding))
-        menu_pulsanti.addWidget(self.get_push_button("Manuale", self.go_to_manuale(), QIcon('icona_manuale.png'), 'Manuale'))
-        menu_pulsanti.setContentsMargins(5,0,5,0)
+        menu_pulsanti.addWidget(self.get_push_button("Manuale", self.go_to_manuale, QIcon('Home\\manual_icon.png'), 'Manuale'))
+        menu_pulsanti.setContentsMargins(4,0,4,0)
 
         orizzontal_layout.addLayout(menu_pulsanti)
         orizzontal_layout.addWidget(label)
+        orizzontal_layout.setContentsMargins(0,0,0,0)
 
-        ext_layout.addWidget(striscia)
-        ext_layout.addLayout(orizzontal_layout)
-        ext_layout.setContentsMargins(0,0,0,5)
+        self.ext_layout.addWidget(striscia)
+        self.ext_layout.addLayout(orizzontal_layout)
+        self.ext_layout.setContentsMargins(0,0,0,0)
 
-        self.setLayout(ext_layout)
+        self.setLayout(self.ext_layout)
+        self.setGeometry(140, 80, 500, 500)
 
 
     #Restituisce un pulsante ed il suo collegamento
     def get_push_button(self, nome, on_click, icon, tooltip):
-        button = QPushButton(nome)
+        button = QPushButton()
         button.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Expanding)
-        #button.clicked.connect(on_click)
-        button.setFont(QFont("Arial Black", 18))
+        button.clicked.connect(on_click)
+        #button.setFont(QFont("Arial Black", 18))
         button.setStyleSheet("QPushButton"                   #stile del pulsante
                             "{"
                             "border-radius: 7px;"
@@ -70,17 +72,20 @@ class VistaHome_amministratore(QWidget):
                              "}"
                              "QPushButton::hover"
                              "{"
-                             "background-color : #00BF7F ;"
+                             "background-color : #00BF7F ;"  
                              "}"
                             )
         button.setIcon(icon)
+        button.setIconSize(QtCore.QSize(70,50))
         button.setToolTip(tooltip)
         button.setToolTipDuration(2200)
         return button
 
     #Funzione che gestiranno i click dei pulsanti
     def go_to_report(self):
-        pass
+        self.ext_layout.removeWidget(label)
+        self.ext_layout.addLayout(GestisciDipendente())
+
 
     def go_to_dipendenti(self):
         pass
